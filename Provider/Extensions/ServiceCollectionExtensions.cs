f0164dde596454b1bc779d63bf6fa3f60caf8546
mohammadsargazi <m.sargazidev@gmail.com>
2023-06-29 15:19:00 +0330

    - Add ServiceCollectionExtensions for Elasticsearch configuration
    This commit message indicates the addition of the ServiceCollectionExtensions class, which provides an extension method ConfigureElasticSearch for configuring Elasticsearch within an IServiceCollection. The method takes an ElasticConfiguration object as a parameter, which includes the necessary configuration settings such as the Elasticsearch server URI, username, and password. Inside the method, the ElasticClient instance is created with the provided configuration settings, and it is registered as a singleton within the service collection. Finally, the modified service collection is returned

---
+++ Provider/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using Provider.Configuration;
+
+
+namespace Provider.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureElasticSearch(this IServiceCollection services, ElasticConfiguration elasticConfiguration)
+    {
+        var uri = new Uri(elasticConfiguration.Uri);
+        services.AddSingleton<IElasticClient>(
+            new ElasticClient(
+                new ConnectionSettings(uri)
+                    .BasicAuthentication(elasticConfiguration.Username, elasticConfiguration.Password)
+            ));
+
+        return services;
+    }
+}
