b2a92648c450e84abfde8c88ef14c66131b5229b
mohammadsargazi <m.sargazidev@gmail.com>
2023-06-29 15:16:40 +0330

    - Add IBaseRepository and IBaseRepository<T> interfaces for repository operations
    This commit message indicates the addition of two interfaces: IBaseRepository and IBaseRepository<T>. The IBaseRepository interface provides a common method CountAsync() to retrieve the count of entities asynchronously. The IBaseRepository<T> interface extends IBaseRepository and includes a set of methods for various repository operations such as CanDelete, DeleteAsync, InsertAsync, InsertAllAsync, UpdateAsync, UpdateAllAsync, IncrementAsync, FirstOrDefaultAsync, SingleOrDefaultAsync, SingleAsync, ListAsync, ListAsync (with optional ids parameter), and ListPagedAsync. These interfaces enable flexible and standardized repository functionality for entities implementing IBaseEntity

---
+++ Provider/Contracts/IBaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using Provider.Results;
+
+namespace Provider.Contracts;
+
+public interface IBaseRepository
+{
+    Task<long> CountAsync();
+}
+
+public interface IBaseRepository<T> : IBaseRepository where T : IBaseEntity
+{
+    Task<CanDeleteResult> CanDelete(T entity, CancellationToken cancellationToken);
+    Task<DeletedResult> DeleteAsync(T entity, CancellationToken cancellationToken);
+    Task<InsertResult<T>> InsertAsync(T entity, CancellationToken cancellationToken);
+    Task<List<InsertResult<T>>> InsertAllAsync(List<T> entities, CancellationToken cancellationToken);
+    Task<UpdatedResult<T>> UpdateAsync(T entity, CancellationToken cancellationToken);
+    Task<List<UpdatedResult<T>>> UpdateAllAsync(List<T> entities, CancellationToken cancellationToken);
+    Task<IncrementResult> IncrementAsync(Guid id, string field, Guid version, double amount = 1);
+    Task<T?> FirstOrDefaultAsync(CancellationToken cancellationToken);
+    Task<T?> SingleOrDefaultAsync(Guid id, CancellationToken cancellationToken);
+    Task<T> SingleAsync(Guid id, CancellationToken cancellationToken);
+    Task<List<T>> ListAsync(CancellationToken cancellationToken);
+    Task<List<T>> ListAsync(List<Guid?> ids, CancellationToken cancellationToken);
+    Task<List<T>> ListAsync(List<Guid> ids, CancellationToken cancellationToken);
+    Task<IPagedData<T>> ListPagedAsync(IPagedRequest request, CancellationToken cancellationToken);
+}
