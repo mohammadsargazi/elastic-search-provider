d1fb215e49e74087ece52e2c95c54de4fd71e324
mohammadsargazi <m.sargazidev@gmail.com>
2023-06-29 14:46:07 +0330

    - Add ElasticConfiguration class

---
+++ Provider/Configuration/ElasticConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Provider.Configuration;
+
+public class ElasticConfiguration
+{
+    public string Uri { get; set; } = "";
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+}
