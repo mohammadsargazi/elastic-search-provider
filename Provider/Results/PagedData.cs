4ef76d020e9aaae3fc6f499acc7a6127bfe8955c
mohammadsargazi <m.sargazidev@gmail.com>
2023-06-29 15:09:59 +0330

    - Implement PagedData class for paginated data storage
    This commit message indicates the implementation of the PagedData class, which serves as a container for storing paginated data. The class implements the IPagedData<T> interface and includes properties Result and TotalCount to hold the paginated data and the total count of items respectively. The class also includes a constructor that allows initializing the Result and TotalCount properties

---
+++ Provider/Results/PagedData.cs
@@ -0,0 +1,12 @@
+﻿using Provider.Contracts;
+
+public class PagedData<T> : IPagedData<T>
+{
+    public PagedData(List<T> result, long totalCount)
+    {
+        Result = result;
+        TotalCount = totalCount;
+    }
+    public List<T> Result { get; set; }
+    public long TotalCount { get; set; }
+}
