28965af4f0ce6b324482ad5d7dea2ec83ba53a4a
mohammadsargazi <m.sargazidev@gmail.com>
2023-06-29 16:19:57 +0330

    - Add BaseElasticRepositoryTests class
    - Added the BaseElasticRepositoryTests class to facilitate unit testing for repositories derived from BaseElasticRepository.
    - The class sets up necessary dependencies and provides a helper method for generating sample entities.

--- Test/Usings.cs
+++ Test/Usings.cs
@@ -1,1 +1,7 @@
 global using Xunit;
+global using Microsoft.Extensions.Localization;
+global using Moq;
+global using Nest;
+global using Provider.Configuration;
+global using Provider.Types.Entities;
+global using Provider.Types.Repositories;
