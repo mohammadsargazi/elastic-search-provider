8fb25019c93472aa11a3fb060706ce85d617bb99
mohammadsargazi <m.sargazidev@gmail.com>
2023-06-29 16:33:52 +0330

    Add unit tests for UpdateAsync method
    - Added unit tests for the UpdateAsync method in TestElasticRepository.
    - The tests verify the behavior of the method when updating a valid entity and when updating invalid entities.
    - The tests ensure that the appropriate update result is returned based on the success or failure of the update operation.
    - Inserts entities before executing the tests and performs update operations on the entities.
    - Includes tests for both single entity update and batch update scenarios.

---
+++ Test/UnitTests/UpdateAsync.cs
@@ -0,0 +1,65 @@
+﻿namespace Test.UnitTests;
+
+
+public class UpdateAsync : BaseElasticRepositoryTests<TestEntity, TestElasticRepository>
+{
+    [Fact]
+    public async Task ValidEntity_ReturnsUpdateResult()
+    {
+        // Arrange
+        var entity = GenerateSampleEntity(Guid.NewGuid());
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        await _repository.InsertAsync(entity, cancellationToken);
+        var updatedEntity = entity with { Title = "UpdatedValue" };
+        var result = await _repository.UpdateAsync(updatedEntity, cancellationToken);
+
+
+        // Assert
+        Assert.True(result.updatedCount > 0);
+    }
+
+    [Fact]
+    public async Task InValidEntity_ReturnsUpdateResultWithZeroCount()
+    {
+        // Arrange
+        var entity = GenerateSampleEntity(Guid.NewGuid());
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        await _repository.InsertAsync(entity, cancellationToken);
+        var updatedEntity = entity with { Title = "UpdatedValue", Version = Guid.NewGuid() };
+
+
+        // Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(() => _repository.UpdateAsync(updatedEntity, cancellationToken));
+    }
+    [Fact]
+    public async Task InValidEntities_ReturnsUpdateResultWithZeroCount()
+    {
+        // Arrange
+        var entities = Enumerable.Range(0, 10)
+        .Select(_ => GenerateSampleEntity(Guid.NewGuid()))
+        .ToList();
+
+        var entities2 = Enumerable.Range(0, 10)
+       .Select(_ => GenerateSampleEntity(Guid.NewGuid()))
+       .ToList();
+
+
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        await _repository.InsertAllAsync(entities, cancellationToken);
+        var updatedEntities = entities.Select(
+            entity => entity with { Title = "UpdatedValue", Version = Guid.NewGuid() }).ToList();
+
+        updatedEntities.AddRange(entities2);
+
+        var result = await _repository.UpdateAllAsync(updatedEntities, cancellationToken);
+
+        // Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(() => _repository.UpdateAllAsync(updatedEntities, cancellationToken));
+    }
+}
